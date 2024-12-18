fef3a3d39a48824a1ea8c61229a46ca4eebbf462
JeremyRoalef <roalef944@morrisville.edu>
2024-12-18 17:24:07 -0500

    Add Initial Commit || 2.2 Start() & Update()

---
+++ ObstacleDodge/Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+
+        transform.Translate(0,0,0.01f);
+    }
+}
