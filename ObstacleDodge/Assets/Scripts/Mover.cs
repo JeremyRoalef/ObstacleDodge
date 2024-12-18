8d2038829880d2711aabc028dfc05743ac053ba7
JeremyRoalef <roalef944@morrisville.edu>
2024-12-18 18:00:49 -0500

    2.11 Practicing With Methods
    2.10 is a theory lecture

--- ObstacleDodge/Assets/Scripts/Mover.cs
+++ ObstacleDodge/Assets/Scripts/Mover.cs
@@ -19,6 +19,11 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         //Time.DeltaTime changes the value based on the change in time from one frame to another.
         //Variable time changes.
         float xValue = Input.GetAxis("Horizontal") * xVelocity * Time.deltaTime;
