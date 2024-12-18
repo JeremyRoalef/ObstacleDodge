22024fa287b537adac61af78ab7d78df0cd8bd69
JeremyRoalef <roalef944@morrisville.edu>
2024-12-18 18:06:32 -0500

    2.13 Using GetComponent()

--- ObstacleDodge/Assets/Scripts/ObjectHit.cs
+++ ObstacleDodge/Assets/Scripts/ObjectHit.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    [SerializeField]
+    Color hitColor = Color.red;
+
+    MeshRenderer objRenderer;
+
+    private void Awake()
+    {
+        objRenderer = GetComponent<MeshRenderer>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log($"{gameObject.name} hit");
+        objRenderer.material.color = hitColor;
     }
 }
